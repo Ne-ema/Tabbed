67975b692b1b838e39462a8a2eee8968a825419d
Ne-ema <neemamutanu@gmail.com>
2022-08-02 11:43:09 +0300

    commit

---
+++ app/src/main/java/com/example/tabbed/CodeFragment.kt
@@ -0,0 +1,16 @@
+package com.example.tabbed
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CodeFragment : Fragment() {
+    // inflate the layout
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) =
+        inflater.inflate(R.layout.fragment_code, container, false)!!
+}
